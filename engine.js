40529af1e010053095e01475607c7b0d57b601d1
Lucius Latham <kingcodefish@gmail.com>
2016-06-05 04:16:14 -0400

    Big strides being made

--- engine.js
+++ engine.js
@@ -1,19 +1,25 @@
 "use strict";
 
-var currRoom;
+var currRoom = undefined;
+var game = {
+    title: "3 Room Adventure",
+    subtitle: "An Interactive Adventure by Black Time Studios, LLC."
+};
 var objects = {
     chamber: {
         type: "room",
         description: "Once, this was of the utmost in decor and ornamentation for someone equally deserving of their high appointment, but this too was long ago. Now, there's a constant chill as this bitter air can be felt gnawing through these walls. An all to real and present smell of death, blight, and other familiars have made their presence known. A man, I've awakened while spread on the floor atop the remnants of an animal fur rug. In this area, the floor maintains a different hue. The walls groan in an uncanny manner that needles a startled expression in my face. My ears close as I stand.",
         player: {
             type: "player",
+            aliases: "player, me",
             description: "Just some dude.",
-            inventory: []
+            inventory: {}
         },
         bucket: {
             type: "object",
-            aliases: "tool",
-            description: "Just a bucket."
+            aliases: "bucket, can",
+            description: "Just a bucket.",
+            visibility: true
         }
     },
     hallway: {
@@ -38,6 +44,17 @@ var objects = {
             }
         }
         delete this.init;
+    },
+    updateObjects: function(object) {
+        $(".tae-objects .tae-menu-wrapper").text(" ");
+        for(var i in objects[currRoom]) {
+            if(objects[currRoom][i].type === "object") {
+                var aliases = objects[currRoom][i].aliases.split(/,\s?/);
+                if (aliases[object !== undefined ? aliases.indexOf(object) : 0] === i && objects[currRoom][i].visibility) {
+                    $("<p>").text(i).appendTo(".tae-objects .tae-menu-wrapper");
+                }
+            }
+        }
     }
 };
 objects.init();
@@ -54,30 +71,43 @@ var commands = {
         }
     },
     "take": function(object) {
-        if (objects[currRoom][object].type === "object") {
-            objects[currRoom].player.inventory.push(objects[currRoom][object]);
-            var currAliases = objects[currRoom][object].aliases.split("/,\s?/");
+        if(objects[currRoom][object].type === "object") {
+            var currAliases = objects[currRoom][object].aliases.split(/,\s?/);
             for(var i = 0; i < currAliases.length; i++) {
-                console.log(currAliases[i]);
+                objects[currRoom].player.inventory[currAliases[i]] = objects[currRoom][currAliases[i]];
                 delete objects[currRoom][currAliases[i]];
             }
-            delete objects[currRoom][object];
             $("<p>").text(object).appendTo(".tae-inventory .tae-menu-wrapper");
+            objects.updateObjects();
             output("You took the " + object);
         } else {
             output("You can't pick that up.");
         }
+    },
+    "drop": function(object) {
+        if(objects[currRoom].player.inventory[object] !== undefined) {
+            var currAliases = objects[currRoom].player.inventory[object].aliases.split(/,\s?/);
+            for(var i = 0; i < currAliases.length; i++) {
+                objects[currRoom][currAliases[i]] = objects[currRoom].player.inventory[currAliases[i]];
+                delete objects[currRoom].player.inventory[currAliases[i]];
+                $(".tae-inventory .tae-menu-wrapper :contains(" + currAliases[i] + ")").remove();
+            }
+            objects.updateObjects(object);
+            output("You dropped the " + object);
+        } else {
+            output("You aren't carrying that.");
+        }
     }
 };
-for(var i = 0; i < objects[currRoom].player.inventory.length; i++) {
-    $("<p>").text(objects[currRoom].player.inventory[i]).appendTo(".tae-inventory .tae-menu-wrapper");
-}
+
+/** --- EVENT LISTENERS --- **/
 $(".tae-input").bind("enterKey", function() {
     var input = $(".tae-input").val().split(/\s(.+)?/);
     input.length > 1 ? input.pop() : 0;
     $("<pre>").text(input[0] + (input[1] !== undefined ? " " + input[1] : "")).appendTo(".tae-output");
-    if (commands[input[0]] !== undefined && objects[currRoom][input[1]] !== undefined
-        || commands[input[0]] !== undefined && input[1] === undefined) {
+    if(commands[input[0]] !== undefined && objects[currRoom][input[1]] !== undefined
+        || commands[input[0]] !== undefined && input[1] === undefined
+        || input[0] === "drop" && objects[currRoom].player.inventory[input[1]] !== undefined) {
         commands[input[0]](input[1]);
     } else if(commands[input[0]] === undefined) {
         output(input[0] + " is not a known command.");
@@ -97,3 +127,13 @@ $(".tae-menu > div").on("click", function() {
     $(this).toggleClass("active");
     $(this).find(".tae-menu-wrapper").slideToggle("fast");
 });
+
+/** --- INITIALIZATION --- **/
+$("<div>").addClass("tae-output-title").text(game.title).appendTo(".tae-output"); // Title Output
+$("<div>").addClass("tae-output-subtitle").text(game.subtitle).appendTo(".tae-output"); // Subtitle Output
+/* --- INVENTORY HANDLING --- */
+for(var i in objects[currRoom].player.inventory) {
+    $("<p>").text(objects[currRoom].player.inventory[i]).appendTo(".tae-inventory .tae-menu-wrapper");
+}
+/* --- OBJECT HANDLING --- */
+objects.updateObjects();
